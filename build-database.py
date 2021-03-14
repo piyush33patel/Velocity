d367cd157da6fa5cf908ea9b086b31c7c9951fec
Piyush Patel <piyush33patel@yahoo.com>
2021-03-15 00:23:28 +0530

    created the database (DDL-statements)

---
+++ build-database.py
@@ -0,0 +1,43 @@
+from mysql.connector import connect
+
+def repeatedThing(attribute):
+    big = ""
+    small = ""
+    digits = ""    
+    for i in range(ord('a'), ord('z')+1):
+        small += f"small_{chr(i)} int,\n"
+        big += f"big_{chr(i)} int,\n"
+    for i in range(ord('0'), ord('9')+1):
+        digits += f"digit_{chr(i)} int,\n"
+
+    total = f"({attribute} varchar(30),\n"
+    total += f"{small}{big}{digits}"
+    total += "comma int,\n"
+    total += "semicolon int,\n"
+    total += "space int,\n"
+    total += "fullstop int,\n"
+    total += f"PRIMARY KEY ({attribute}))"
+    return total
+
+mydb = connect(host="localhost", user="root", passwd="", database="velocity")
+print(mydb)
+cursor = mydb.cursor()
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS users(
+    name varchar(20) NOT NULL,
+    email varchar(30) NOT NULL,
+    PRIMARY KEY (email)
+    )''')
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS transactions(
+    email varchar(30) NOT NULL,
+    keylog_id varchar(30),
+    PRIMARY KEY (keylog_id),
+    FOREIGN KEY (email) REFERENCES users(email)
+    )''')
+
+cursor.execute(f"CREATE TABLE IF NOT EXISTS paragraphs {repeatedThing('para_number')}")
+cursor.execute(f"CREATE TABLE IF NOT EXISTS keys_pressed {repeatedThing('keylog_id')}")
+cursor.execute(f"CREATE TABLE IF NOT EXISTS errors_made {repeatedThing('keylog_id')}")
+
+mydb.close()
